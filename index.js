6fe9f21f64c2cf32bcc58a020fdbecc32953c3b4
Nickolas <nickolasalcazar@gmail.com>
2024-02-18 19:24:03 -0800

    Successful query to API

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+
+app.listen(3000);
+
+require("dotenv").config();
+
+const API_KEY = process.env.API_KEY;
+const API_URL = process.env.API_URL;
+
+console.log(API_URL + "/villagers");
+
+fetch(API_URL + "/villagers", {
+  method: "GET",
+  headers: { "X-API-KEY": API_KEY, "Accept-Version": "1.6.0" },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));
