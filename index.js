59cf59adb467b392695a684755c8ed5a8a9c026f
Nickolas <nickolasalcazar@gmail.com>
2024-02-19 13:52:45 -0800

    Successfully hits takes requests, hits API and then returns response to frontend

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
 const express = require("express");
 const app = express();
+const PORT = 3000;
+const fs = require("fs");
 
-app.listen(3000);
+app.listen(PORT);
 
 require("dotenv").config();
 
+const NODE_ENV = process.env.NODE_ENV;
 const API_KEY = process.env.API_KEY;
 const API_URL = process.env.API_URL;
 
-console.log(API_URL + "/villagers");
+// const furniture = require("./furniture.json");
+// console.log(furniture.length);
 
-fetch(API_URL + "/villagers", {
-  method: "GET",
-  headers: { "X-API-KEY": API_KEY, "Accept-Version": "1.6.0" },
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+const possiblePaths = ["villagers", "furniture", "fish", "bugs", "art"];
+app.get("*", async (req, res) => {
+  const path = req.path.substring(1);
+  const pathIsValid = possiblePaths.includes(path);
+  if (!pathIsValid) {
+    res.status(404).send("Not found");
+    return;
+  }
+  getResource(path).then((resource) => {
+    if (resource === null) res.sendStatus(500);
+    else res.status(200).json(resource);
+  });
+});
+
+// Get a resource from the API
+async function getResource(path) {
+  const resource = path === "villagers" ? "villagers" : `nh/${path}`;
+  console.log("Fetching resource:", API_URL + resource);
+
+  try {
+    const response = await fetch(API_URL + resource, {
+      method: "GET",
+      headers: { "X-API-KEY": API_KEY, "Accept-Version": "1.6.0" },
+    });
+    const data = await response.json();
+    if (NODE_ENV === "dev") {
+      console.log("Writing to api_response.json...");
+      fs.writeFile(
+        "api_response.json",
+        JSON.stringify(data, null, 4),
+        (error) => {
+          if (error) console.error("Error writing to file:", error);
+        }
+      );
+      return data;
+    }
+  } catch (error) {
+    console.error("Error fetching resource:", error);
+    return null;
+  }
+}
