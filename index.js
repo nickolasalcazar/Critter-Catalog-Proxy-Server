d7c0a0126a7f2f6a056a84e21f3fe70610651b3e
Nickolas <nickolasalcazar@gmail.com>
2024-04-17 08:29:13 -0700

    Minified and stored furniture file locally; server responds with JSON from file when furniture path is requested

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const NODE_ENV = process.env.NODE_ENV;
 const API_KEY = process.env.API_KEY;
 const API_URL = process.env.API_URL;
 
-// const furniture = require("./furniture.json");
-// console.log(furniture.length);
+const furniture = require("./furniture_minified.json");
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3001");
@@ -18,6 +17,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Content-Type", "application/json");
   next();
 });
 
@@ -40,6 +40,10 @@ console.log("Listening on port", PORT);
 
 // Get a resource from the API
 async function getResource(path) {
+  if (path === "furniture") {
+    return furniture;
+  }
+
   const resource = path === "villagers" ? "villagers" : `nh/${path}`;
   console.log("Fetching resource:", API_URL + resource);
 
@@ -48,12 +52,48 @@ async function getResource(path) {
       method: "GET",
       headers: { "X-API-KEY": API_KEY, "Accept-Version": "1.6.0" },
     });
+
     const data = await response.json();
+
     if (NODE_ENV === "dev") {
+      let minify = false;
+      if (path === "furniture") {
+        minify = true;
+        data.forEach((furniture) => {
+          delete furniture.functions;
+          delete furniture.hha_base;
+          delete furniture.hha_category;
+          delete furniture.lucky;
+          delete furniture.lucky_season;
+          delete furniture.item_set;
+          delete furniture.item_series;
+          delete furniture.customizable;
+          delete furniture.custom_kits;
+          delete furniture.custom_kit_type;
+          delete furniture.custom_body_part;
+          delete furniture.custom_pattern_part;
+          delete furniture.height;
+          delete furniture.door_decor;
+          delete furniture.version_added;
+          delete furniture.unlocked;
+          delete furniture.notes;
+          delete furniture.variation_total;
+          delete furniture.pattern_total;
+          delete furniture.grid_width;
+          delete furniture.grid_length;
+
+          furniture.variations.forEach((variation) => {
+            delete variation.variation;
+            delete variation.pattern;
+            delete variation.colors;
+          });
+        });
+      }
+
       console.log("Writing to api_response.json...");
       fs.writeFile(
         "api_response.json",
-        JSON.stringify(data, null, 4),
+        JSON.stringify(data, null, minify ? 0 : 4),
         (error) => {
           if (error) console.error("Error writing to file:", error);
           else console.log("Successfully wrote to api_response.json");
