f4ec959e1a7ff7a82f4e5a12c82c4c4397f96219
Nickolas <nickolasalcazar@gmail.com>
2024-04-24 18:26:31 -0700

    Added extra attibute to furniture objects -- "customizable"

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT;
 const NODE_ENV = process.env.NODE_ENV;
 const API_KEY = process.env.API_KEY;
 const API_URL = process.env.API_URL;
+const OUTPUT_FILE = "api_response.json";
 
 const furniture = require("./furniture_minified.json");
 
@@ -40,9 +41,7 @@ console.log("Listening on port", PORT);
 
 // Get a resource from the API
 async function getResource(path) {
-  if (path === "furniture") {
-    return furniture;
-  }
+  if (path === "furniture") return furniture;
 
   const resource = path === "villagers" ? "villagers" : `nh/${path}`;
   console.log("Fetching resource:", API_URL + resource);
@@ -55,54 +54,61 @@ async function getResource(path) {
 
     const data = await response.json();
 
+    // If running in dev mode, output API response to OUTPUT_FILE
     if (NODE_ENV === "dev") {
       let minify = false;
       if (path === "furniture") {
         minify = true;
-        data.forEach((furniture) => {
-          delete furniture.functions;
-          delete furniture.hha_base;
-          delete furniture.hha_category;
-          delete furniture.lucky;
-          delete furniture.lucky_season;
-          delete furniture.item_set;
-          delete furniture.item_series;
-          delete furniture.customizable;
-          delete furniture.custom_kits;
-          delete furniture.custom_kit_type;
-          delete furniture.custom_body_part;
-          delete furniture.custom_pattern_part;
-          delete furniture.height;
-          delete furniture.door_decor;
-          delete furniture.version_added;
-          delete furniture.unlocked;
-          delete furniture.notes;
-          delete furniture.variation_total;
-          delete furniture.pattern_total;
-          delete furniture.grid_width;
-          delete furniture.grid_length;
-
-          furniture.variations.forEach((variation) => {
-            delete variation.variation;
-            delete variation.pattern;
-            delete variation.colors;
-          });
-        });
+        trimFurniture(data);
       }
-
-      console.log("Writing to api_response.json...");
+      console.log(`Writing to ${OUTPUT_FILE}...`);
       fs.writeFile(
-        "api_response.json",
+        OUTPUT_FILE,
         JSON.stringify(data, null, minify ? 0 : 4),
         (error) => {
-          if (error) console.error("Error writing to file:", error);
-          else console.log("Successfully wrote to api_response.json");
+          if (error) console.error(`Error writing to ${OUTPUT_FILE}:`, error);
+          else console.log(`Successfully wrote to ${OUTPUT_FILE}`);
         }
       );
-      return data;
     }
+
+    return data;
   } catch (error) {
     console.error("Error fetching resource:", error);
     return null;
   }
 }
+
+// Helper function for trimming furniture object that is returned by API.
+// Only used in development.
+const trimFurniture = (data) => {
+  data.forEach((furniture) => {
+    delete furniture.url;
+    delete furniture.functions;
+    delete furniture.hha_base;
+    delete furniture.hha_category;
+    delete furniture.lucky;
+    delete furniture.lucky_season;
+    delete furniture.item_set;
+    delete furniture.item_series;
+    delete furniture.custom_kits;
+    delete furniture.custom_kit_type;
+    delete furniture.custom_body_part;
+    delete furniture.custom_pattern_part;
+    delete furniture.height;
+    delete furniture.door_decor;
+    delete furniture.version_added;
+    delete furniture.unlocked;
+    delete furniture.notes;
+    delete furniture.variation_total;
+    delete furniture.pattern_total;
+    delete furniture.grid_width;
+    delete furniture.grid_length;
+
+    furniture.variations.forEach((variation) => {
+      delete variation.variation;
+      delete variation.pattern;
+      delete variation.colors;
+    });
+  });
+};
